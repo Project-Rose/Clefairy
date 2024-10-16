941613913b12688def390df8870ca366236314d0
Miguel F. <miguelbragafogli@hotmail.com>
2024-10-16 04:05:11 +0000

    MIi QR code generation babyyyy

--- commands/utility/mii.ts
+++ commands/utility/mii.ts
@@ -1,20 +1,23 @@
 import { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { encryptAesCcm } from "../libs/mii.cjs";
+import * as QRCode from "npm:qrcode";
+import { Buffer } from "node:buffer";
 
 export const data = new SlashCommandBuilder()
   .setName('mii')
   .setDescription('Fetch a Mii using the Mii-Unsecure API')
-  .addStringOption(option => 
-    option.setName('render') 
-      .setDescription('Select the render that you want to use. The default one is WiiU/Miiverse.')
+  .addStringOption(option =>
+    option.setName('render')
+      .setDescription('Select the render you want to use (default: Wii U/Miiverse).')
       .setRequired(true)
       .addChoices(
         { name: 'Wii U/Miiverse', value: 'wiiu' },
         { name: 'Switch', value: 'switch' },
         { name: 'Miitomo', value: 'miitomo' }
       ))
-  .addStringOption(option => 
-    option.setName('expression') 
-      .setDescription('Select the expression that you want to use. The default one is the Normal expression.')
+  .addStringOption(option =>
+    option.setName('expression')
+      .setDescription('Select the expression you want to use (default: Normal).')
       .setRequired(true)
       .addChoices(
         { name: 'Normal', value: 'normal' },
@@ -36,11 +39,11 @@ export const data = new SlashCommandBuilder()
         { name: 'Wink (left eye open and smiling)', value: 'like_wink_left' },
         { name: 'Wink (right eye open and smiling)', value: 'like_wink_right' },
         { name: 'Frustrated', value: 'frustrated' },
-        { name: '🥺', value: '🥺' }        
+        { name: '🥺', value: '🥺' }
       ))
-  .addStringOption(option => 
+  .addStringOption(option =>
     option.setName('type')
-      .setDescription('Select how your Mii will be positioned. The default is Portrait.')
+      .setDescription('Select how your Mii will be positioned (default: Portrait).')
       .setRequired(true)
       .addChoices(
         { name: 'Portrait', value: 'face' },
@@ -49,87 +52,95 @@ export const data = new SlashCommandBuilder()
         { name: 'Whole Body', value: 'all_body' },
         { name: 'Switch Portrait', value: 'variableiconbody' }
       ))
-  .addStringOption(option => 
+  .addStringOption(option =>
     option.setName('pnid')
       .setDescription('The PNID of the Mii to fetch. Leave blank if using NNID.')
       .setRequired(false))
   .addStringOption(option =>
     option.setName('nnid')
-      .setDescription('The NNID of the Mii to fetch. Will only show Miis as they were in 4/8/24. Leave blank if using PNID.')
+      .setDescription('The NNID of the Mii to fetch. Leave blank if using PNID.')
       .setRequired(false));
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-    const miinnid = interaction.options.getString('nnid');
-    const miipnid = interaction.options.getString('pnid');
-    const miiRender = interaction.options.getString('render')!;
-    const miiExpression = interaction.options.getString('expression')!;
-    const miiType = interaction.options.getString('type')!;
-    
-    let nid: string | null = null;
-    let api_id: number;
-    let shaderType: number;
+  const miinnid = interaction.options.getString('nnid');
+  const miipnid = interaction.options.getString('pnid');
+  const miiRender = interaction.options.getString('render')!;
+  const miiExpression = interaction.options.getString('expression')!;
+  const miiType = interaction.options.getString('type')!;
+  
+  const nid = miipnid || miinnid;
+  const api_id = miipnid ? 1 : 0;
 
-    switch (miiRender) {
-        case 'wiiu': shaderType = 3; break;
-        case 'miitomo': shaderType = 2; break;
-        case 'switch': shaderType = 1; break;
-        default: return;
+  if (!nid || (miipnid && miinnid)) {
+    return interaction.reply({ content: "Please enter either a PNID or NNID, but not both.", ephemeral: true });
+  }
+
+  const shaderType = { wiiu: 3, miitomo: 2, switch: 1 }[miiRender] || 3;
+
+  const miiImageURL = `https://mii-unsecure.ariankordi.net/miis/image.png?nnid=${nid}&api_id=${api_id}&type=${miiType}&width=1080&expression=${miiExpression}&shaderType=${shaderType}`;
+  const miiDataURL = `https://mii-unsecure.ariankordi.net/mii_data/${nid}?api_id=${api_id}`;
+
+  try {
+    const [imageResponse, dataResponse] = await Promise.all([fetch(miiImageURL), fetch(miiDataURL)]);
+
+    if (!imageResponse.ok) {
+      return interaction.reply({ content: "I couldn't find your Mii, was it all just in your imagination perhaps?.", ephemeral: true });
     }
 
-    if (miipnid && !miinnid) {
-        api_id = 1; // PNID
-        nid = miipnid;
-    } else if (miinnid && !miipnid) {
-        api_id = 0; // NNID
-        nid = miinnid;
-    } else {
-        return await interaction.reply({ content: "Pleeeeeeeeeease enter either a PNID or NNID, but not both, or i'm going to cry.", ephemeral: true });
-    }
+    const imageStream = imageResponse.body;
+    const attachment = new AttachmentBuilder(imageStream, { name: 'mii.png' });
+    const miiData = await dataResponse.json();
+    const { pid, images, studio_url_data, ...filteredData } = miiData;
+    const QrData = miiData.data;
 
-    // Endpoints
-    const miiImageURL = `https://mii-unsecure.ariankordi.net/miis/image.png?nnid=${nid}&api_id=${api_id}&type=${miiType}&width=1080&expression=${miiExpression}&shaderType=${shaderType}`;
-    const miiDataURL = `https://mii-unsecure.ariankordi.net/mii_data/${nid}?api_id=${api_id}`;
+    const miiQrStream = await generateQrCode(QrData);
+    const MiiQrAttachment = new AttachmentBuilder(miiQrStream, { name: 'mii_qr.png' });
 
-    try {
-        const [imageResponse, dataResponse] = await Promise.all([
-            fetch(miiImageURL),
-            fetch(miiDataURL)
-        ]);
+    const embed = buildMiiEmbed(miiData, api_id);
+    await interaction.reply({ embeds: [embed], files: [attachment, MiiQrAttachment] });
 
-        if (!imageResponse.ok) {
-            return interaction.reply({ content: 'Mii not found. Are you sure it exists outside of your imagination world?', ephemeral: true });
-        }
+  } catch (error) {
+    console.error('Error fetching Mii:', error);
+    await interaction.reply({ content: "Couldn't fetch your sillyyyy Mii for some reason. Please try again.", ephemeral: true });
+  }
+}
 
-        const imageStream = await imageResponse.body;
-        const attachment = new AttachmentBuilder(imageStream, { name: 'mii.png' });
-        const miiData = await dataResponse.json();
-        const { data, pid, images, studio_url_data, ...filteredData } = miiData;
-        
-        const networkInfo = api_id === 1
-            ? { idType: 'PNID', color: '#1b1f3b', text: 'Environment: Pretendo Network', favicon: 'https://pretendo.network/assets/images/icons/favicon-32x32.png' }
-            : { idType: 'NNID', color: '#FF7D00', text: 'Environment: Nintendo Network', favicon: 'https://media.discordapp.net/attachments/1290013025633304696/1295241468625158144/favicon.png?ex=670def1e&is=670c9d9e&hm=df87ca7c20587e08f6c20c0dad9a09045836c8754f18d0e0c99165a3a90b2f93&=&format=webp&quality=lossless' };
+async function generateQrCode(data: string) {
+  const decodedData = new Uint8Array(Buffer.from(data, 'base64'));
+  const encrypted = encryptAesCcm(decodedData);
+  const qrStream = await QRCode.toBuffer([{ data: encrypted, mode: 'byte' }]);
+  return qrStream;
+}
 
-        const prettyNames: { [key: string]: string } = {
-            name: "Name",
-            user_id: networkInfo.idType,
-        };
+function buildMiiEmbed(miiData: any, api_id: number) {
+  const networkInfo = api_id === 1
+    ? { idType: 'PNID', color: '#1b1f3b', text: 'Environment: Pretendo Network', favicon: 'https://pretendo.network/assets/images/icons/favicon-32x32.png' }
+    : { idType: 'NNID', color: '#FF7D00', text: 'Environment: Nintendo Network', favicon: 'https://media.discordapp.net/attachments/1290013025633304696/1295610730904817684/image.png?ex=670fefc5&is=670e9e45&hm=82d1fd7070057021150928c3410eed859c0feac65aabd30dbe6be6f064ff76ac&=&format=webp&quality=lossless' };
 
-        const embed = new EmbedBuilder()
-            .setColor(networkInfo.color)
-            .setTitle(`${miiData.name}'s Mii`)
-            .setDescription('Here are the details for your really silly and cute Mii:')
-            .setThumbnail('attachment://mii.png')
-            .setFooter({ text: networkInfo.text, iconURL: networkInfo.favicon });
+    const embed = new EmbedBuilder()
+    .setColor(networkInfo.color)
+    .setTitle(`${miiData.name}'s Mii`)
+    .setDescription('Here are the details for your silly ass Mii.')
+    .setThumbnail('attachment://mii.png')
+    .setFooter({ text: networkInfo.text, iconURL: networkInfo.favicon });
+  
+  const prettyNames = {
+    name: "Name",
+    user_id: networkInfo.idType,
+  };
+  
+  const filteredData = (({ name, user_id }) => ({
+    name, user_id
+  }))(miiData);
+  
+  for (const [key, value] of Object.entries(filteredData)) {
+    const prettyKey = prettyNames[key] || key;
+    embed.addFields({ name: prettyKey, value: String(value), inline: true });
+  }
+  
+  embed.addFields({ name: 'QR Code:', value: '\u200B', inline: false });
 
-         for (const [key, value] of Object.entries(filteredData)) {
-            const prettyKey = prettyNames[key] || key; 
-            embed.addFields({ name: prettyKey, value: String(value), inline: true });
-        }
+  embed.setImage('attachment://mii_qr.png');  
 
-        await interaction.reply({ embeds: [embed], files: [attachment] });
-
-    } catch (error) {
-        console.error('Error fetching Mii:', error);
-        await interaction.reply({ content: "I couldn't fetch your Mii for some goddamm reason. Please try again or something idk you do you", ephemeral: true });
-    }
+  return embed;
 }
